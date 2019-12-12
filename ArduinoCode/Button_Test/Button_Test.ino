d37c85c2a244b2b580a2ce33ad75fe9390201ff7
Po Hsiang Huang <phh003@ucsd.edu>
2019-12-12 00:50:58 -0800

    BLE and Button Test added

---
+++ ArduinoCode/Button_Test/Button_Test.ino
@@ -0,0 +1,25 @@
+const int button1Pin = 11;
+const int button2Pin = 12;
+
+int button1State = 0;
+int button2State = 0;
+
+void setup() {
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+  //Serial.println(button1State);
+  //Serial.println(button2State);
+
+  if (button1State == HIGH) {
+    Serial.println("button 1 pressed");
+  }
+  if (button2State == HIGH) {
+    Serial.println("button 2 pressed");
+  }
+}
